0350df3ec6bcbe2c9b939794184aedadbef17466
Damian Martin <damianmd251@gmail.com>
2021-11-08 20:12:13 +0100

    Ejercicio 414 - 415

--- Autenticación/412peliculas.php
+++ Autenticación/412peliculas.php
@@ -1,31 +1,51 @@
-<?php 
+<?php
 /* 412peliculas.php: vista que muestra como título "Listado de Películas", y una lista desordenada con tres películas. */
-    // Recuperamos la información de la sesión
-    if(!isset($_SESSION)) {
-        session_start();
-    }
+// Recuperamos la información de la sesión
+if (!isset($_SESSION)) {
+    session_start();
+}
 
-    // Y comprobamos que el usuario se haya autentificado
-    if (!isset($_SESSION['usuario'])) {
-       die("Error - debe <a href='index.php'>identificarse</a>.<br />");
-    }
+// Y comprobamos que el usuario se haya autentificado
+if (!isset($_SESSION['usuario'])) {
+    die("Error - debe <a href='index.php'>identificarse</a>.<br />");
+}
+
+if (!isset($_SESSION["peliculas"])) {
+    $_SESSION["peliculas"][] = "Eternals";
+    $_SESSION["peliculas"][] = "Venom: Let There Be Carnage";
+    $_SESSION["peliculas"][] = "The Last Duel";
+}
+
+$peliculas = $_SESSION["peliculas"];
+
 ?>
 <!DOCTYPE html>
 <html lang="es">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Listado de películas</title>
 </head>
+
 <body>
+    <header>
+        <nav>
+            <ul>
+                <li><a href="412peliculas.php">Películas</a></li>
+                <li><a href="414series.php">Series</a></li>
+            </ul>
+        </nav>
+    </header>
     <h1>Bienvenido <?= $_SESSION['usuario'] ?></h1>
     <p>Pulse <a href="413logout.php">aquí</a> para salir</p>
     <p>Volver al <a href="412peliculas.php">inicio</a></p>
     <h2>Listado de películas</h2>
     <ul>
-        <li>Eternals</li>
-        <li>Venom: Let There Be Carnage</li>
-        <li>The Last Duel</li>
+        <?php foreach ($peliculas as $pelicula) { ?>
+            <li><?= $pelicula ?></li>
+        <?php } ?>
     </ul>
 </body>
+
 </html>
---
+++ Autenticación/414series.php
@@ -0,0 +1,70 @@
+<?php
+/* 414series.php: Añade un nueva vista similar a 412peliculas.php 
+que muestra un "Listado de Series" con una lista desordenada con tres series. 
+Tanto 412pelicuas.php como la vista recien creadas, deben tener un pequeño menú (sencillo, mediante enlaces) 
+que permita pasar de un listado a otro. Comprueba que si se accede directamente a cualquiera de las vistas 
+sin tener un usuario logueado via URL del navegador, no se muestra el listado. */
+
+/*
+Modifica tanto el controlador como las vistas para que:
+
+    los datos los obtenga el controlador (almacena en la sesión un array de películas y otro de series)
+    coloque los datos en la sesión
+    En las vistas, los datos se recuperan de la sesión y se pintan en la lista desordenada
+     recorriendo el array correspondiente.
+
+*/
+
+if (!isset($_SESSION)) {
+    session_start();
+}
+
+// Y comprobamos que el usuario se haya autentificado
+if (!isset($_SESSION['usuario'])) {
+    die("Error - debe <a href='410index.html'>identificarse</a>.<br />");
+}
+
+if (!isset($_SESSION["series"])) {
+    $_SESSION["series"][] = "Black Mirror";
+    $_SESSION["series"][] = "House of Cards";
+    $_SESSION["series"][] = "Orange is the New Black";
+}
+
+
+$series = $_SESSION["series"];
+
+/* echo var_dump($series);
+echo var_dump($_SESSION["series"]); */
+
+?>
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Listado de series</title>
+</head>
+
+<body>
+    <header>
+        <nav>
+            <ul>
+                <li><a href="412peliculas.php">Películas</a></li>
+                <li><a href="414series.php">Series</a></li>
+            </ul>
+        </nav>
+    </header>
+    <h1>Bienvenido <?= $_SESSION['usuario'] ?></h1>
+    <p>Pulse <a href="413logout.php">aquí</a> para salir</p>
+    <p>Volver al <a href="412peliculas.php">inicio</a></p>
+    <h2>Listado de series</h2>
+    <ul>
+        <?php foreach ($series as $serie) { ?>
+            <li><?= $serie ?></li>
+        <?php } ?>
+
+    </ul>
+</body>
+
+</html>
